a9adf6777bfb7d03f41ae421d003afd7a5630ec0
JffMv <90947296+JffMv@users.noreply.github.com>
2025-02-15 16:23:55 +0000

    update principles of isp and ocp

--- src/main/java/com/example/solid/lsp/Car.java
+++ src/main/java/com/example/solid/lsp/Car.java
@@ -1,16 +1,18 @@
 package com.example.solid.lsp;
-
-public class Car {
+import com.example.solid.lsp.interfaces.*;
+public class Car implements Driveble, Refuelable{
+    @Override
     public void drive() {
         System.out.println("Conduciendo...");
     }
 
+    @Override
     public void refuel() {
         System.out.println("Repostando...");
     }
 }
 
-class ElectricCar extends Car {
+class ElectricCar implements Refuelable {
     @Override
     public void refuel() {
         throw new UnsupportedOperationException("Los coches eléctricos no usan combustible.");
---
+++ src/main/java/com/example/solid/lsp/interfaces/drive.java
@@ -0,0 +1,5 @@
+package com.example.solid.lsp.interfaces;
+
+public interface Driveble {
+    void drive();
+}

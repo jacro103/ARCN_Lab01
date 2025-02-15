efed17f22cad7ea269ef4f1ccfb4134528394b92
jacro103 <89158012+jacro103@users.noreply.github.com>
2025-02-15 15:44:55 +0000

    Ejercicios Solid

---
+++ src/main/java/com/example/solid/ocp/DiscountCalculator.java
@@ -0,0 +1,10 @@
+public class DiscountCalculator {
+    public double calculateDiscount(String customerType, double price) {
+        if (customerType.equals("Regular")) {
+            return price * 0.10;
+        } else if (customerType.equals("VIP")) {
+            return price * 0.20;
+        }
+        return 0;
+    }
+}
---
+++ src/main/java/com/example/solid/srp/Invoice .java
@@ -0,0 +1,22 @@
+public class Invoice {
+    private String customer;
+    private double amount;
+
+    public Invoice(String customer, double amount) {
+        this.customer = customer;
+        this.amount = amount;
+    }
+
+    public double calculateTotal() {
+        return amount * 1.21;
+    }
+
+    public void printInvoice() {
+        System.out.println("Factura para: " + customer);
+        System.out.println("Total: " + calculateTotal());
+    }
+
+    public void saveToDatabase() {
+        System.out.println("Guardando factura...");
+    }
+}
